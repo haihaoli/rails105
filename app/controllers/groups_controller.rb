d7bb5d6979a0dfe31b87af8de87baa40741b580c
林晓曼 <linxiaoman@linxiaomandeMacBook-Pro.local>
2017-06-11 20:24:06 +0800

    save

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,5 +2,52 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
-  
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to groups_path
+      flash[:notice] = "Group create"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path
+      flash[:notice] = "Update success"
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    if @group.destroy
+      flash[:alert] = "Group deleted"
+      redirect_to groups_path
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
+
 end
